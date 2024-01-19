2751b40c0a55dcd35999e7ea9c976553dccd9979
dokyung-kang <kangdk02@naver.com>
2024-01-19 16:39:56 +0900

    :sparkles: Add 약관 체크 기능
    약관 전체 동의/취소 기능, 하나씩 동의/취소 기능 추가  Related to : #7

--- app/src/main/java/com/umc/anddeul/start/terms/TermsActivity.kt
+++ app/src/main/java/com/umc/anddeul/start/terms/TermsActivity.kt
@@ -1,11 +1,13 @@
 package com.umc.anddeul.start.terms
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.anddeul.databinding.ActivityTermsBinding
 
 class TermsActivity: AppCompatActivity()  {
     private lateinit var binding: ActivityTermsBinding
+    private var termsCheckedCnt : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +16,111 @@ class TermsActivity: AppCompatActivity()  {
 
         setContentView(binding.root)
 
+        //// 약관 체크 버튼
+
+        // 전체 동의
+        binding.termTotalNotCheckedBtn.setOnClickListener {
+            binding.termChecked1.visibility = View.VISIBLE
+            binding.termChecked2.visibility = View.VISIBLE
+            binding.termChecked3.visibility = View.VISIBLE
+
+            binding.termNotChecked1.visibility = View.GONE
+            binding.termNotChecked2.visibility = View.GONE
+            binding.termNotChecked3.visibility = View.GONE
+
+            termsCheckedCnt = 3
+            isAllChecked()
+        }
+
+        // 전체 동의 취소
+        binding.termTotalCheckedBtn.setOnClickListener {
+            binding.termChecked1.visibility = View.GONE
+            binding.termChecked2.visibility = View.GONE
+            binding.termChecked3.visibility = View.GONE
+
+            binding.termNotChecked1.visibility = View.VISIBLE
+            binding.termNotChecked2.visibility = View.VISIBLE
+            binding.termNotChecked3.visibility = View.VISIBLE
+
+            termsCheckedCnt = 0
+            isAllChecked()
+        }
+
+        // 첫번째 약관 동의
+        binding.termNotChecked1.setOnClickListener {
+            binding.termChecked1.visibility = View.VISIBLE
+            binding.termNotChecked1.visibility = View.GONE
+
+            termsCheckedCnt += 1
+            isAllChecked()
+        }
+
+        // 첫번째 약관 동의 취소
+        binding.termChecked1.setOnClickListener {
+            binding.termChecked1.visibility = View.GONE
+            binding.termNotChecked1.visibility = View.VISIBLE
+
+            termsCheckedCnt -= 1
+            isAllChecked()
+        }
+
+        // 두번째 약관 동의
+        binding.termNotChecked2.setOnClickListener {
+            binding.termChecked2.visibility = View.VISIBLE
+            binding.termNotChecked2.visibility = View.GONE
+
+            termsCheckedCnt += 1
+            isAllChecked()
+        }
+
+        // 두번째 약관 동의 취소
+        binding.termChecked2.setOnClickListener {
+            binding.termChecked2.visibility = View.GONE
+            binding.termNotChecked2.visibility = View.VISIBLE
+
+            termsCheckedCnt -= 1
+            isAllChecked()
+        }
+
+        // 세번째 약관 동의
+        binding.termNotChecked3.setOnClickListener {
+            binding.termChecked3.visibility = View.VISIBLE
+            binding.termNotChecked3.visibility = View.GONE
+
+            termsCheckedCnt += 1
+            isAllChecked()
+        }
+
+        // 세번째 약관 동의 취소
+        binding.termChecked3.setOnClickListener {
+            binding.termChecked3.visibility = View.GONE
+            binding.termNotChecked3.visibility = View.VISIBLE
+
+            termsCheckedCnt -= 1
+            isAllChecked()
+        }
+        
+
         //// 동의 완료 버튼
         binding.termsAgreeBtn.setOnClickListener {
 
         }
 
     }
+    
+    // 전체 체크 여부 확인 함수
+    private fun isAllChecked(){
+        // 전체 체크되었을 때
+        if (termsCheckedCnt == 3){
+            binding.termTotalNotCheckedBtn.visibility = View.GONE
+            binding.termTotalCheckedBtn.visibility = View.VISIBLE
+        }
+
+        // 전체 체크되지 않았을 때
+        if (termsCheckedCnt != 3){
+            binding.termTotalNotCheckedBtn.visibility = View.VISIBLE
+            binding.termTotalCheckedBtn.visibility = View.GONE
+        }
+    }
+    
 }
