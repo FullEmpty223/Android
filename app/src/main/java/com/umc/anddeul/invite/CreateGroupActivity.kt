b9b6d10ab42bcc81de99552ca417c4a0033a3318
dokyung-kang <kangdk02@naver.com>
2024-01-21 20:29:13 +0900

    :lipstick: Add 그룹 만들기 UI
    CreateGroupActivity.kt,, activity_create_group.xml, 상단 png, 버튼 png 추가 Realted to: #8

---
+++ app/src/main/java/com/umc/anddeul/invite/CreateGroupActivity.kt
@@ -0,0 +1,22 @@
+package com.umc.anddeul.invite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.anddeul.databinding.ActivityCreateGroupBinding
+
+class CreateGroupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateGroupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCreateGroupBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //// 새로운 그룹 만들기
+        binding.groupNmBtn.setOnClickListener {
+        }
+
+    }
+}
