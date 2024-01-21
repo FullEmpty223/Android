b495040b0d1edb993efbc780bd5975d91dc846ee
dokyung-kang <kangdk02@naver.com>
2024-01-21 20:55:41 +0900

    :lipstick: Add 초대코드 입력 UI
    JoinGroupActivity.kt,, activity_join_group.xml 추가 Realted to: #8

---
+++ app/src/main/java/com/umc/anddeul/invite/JoinGroupActivity.kt
@@ -0,0 +1,22 @@
+package com.umc.anddeul.invite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.anddeul.databinding.ActivityJoinGroupBinding
+
+class JoinGroupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityJoinGroupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityJoinGroupBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //// 기존 그룹에 참여하기
+        binding.existGroupNmBtn.setOnClickListener {
+        }
+
+    }
+}
