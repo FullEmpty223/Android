652f7851fbc7bd24d201b403a571db5e7c77bab1
Lee <lje2000lje@naver.com>
2024-01-30 02:31:47 +0900

    :closed_lock_with_key: Add 갤러리 접근 권한 추가
    AndroidManifest.xml에 갤러리, 카메라 접근 권한 추가 Related to : #25

---
+++ app/src/main/java/com/umc/anddeul/home/PostWriteActivity.kt
@@ -0,0 +1,11 @@
+package com.umc.anddeul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_write)
+    }
+}
