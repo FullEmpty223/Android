da83e25f413820dda3bb676478a2cab3417e8283
Lee <lje2000lje@naver.com>
2024-01-30 02:52:00 +0900

    :closed_lock_with_key: Update library, key value
    Related to : #25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,11 +48,6 @@ dependencies {
     // swipe refresh layout 라이브러리
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-    implementation ("io.coil-kt:coil:1.4.0")
-
-    // image picker 라이브러리
-    implementation ("io.github.ParkSangGwon:tedimagepicker:1.4.2")
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
