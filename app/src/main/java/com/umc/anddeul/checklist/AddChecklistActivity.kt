2f1fd0f91762599d5e78dce1ada53c2244eb8682
mhyeon-kim <lovelypsent@gmail.com>
2024-02-03 15:18:49 +0900

    :lipstick: Changes png 파일
    png 파일들 vector 파일로 교체 Related to #20

--- app/src/main/java/com/umc/anddeul/checklist/AddChecklistActivity.kt
+++ app/src/main/java/com/umc/anddeul/checklist/AddChecklistActivity.kt
@@ -1,4 +1,19 @@
 package com.umc.anddeul.checklist
 
-class AddChecklistActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.anddeul.databinding.ActivityAddChecklistBinding
+
+class AddChecklistActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAddChecklistBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        binding = ActivityAddChecklistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
 }
