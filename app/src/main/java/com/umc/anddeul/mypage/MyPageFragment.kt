42ccd3df016ef6e49c66de13c5d42d8d6aa762f6
Lee <lje2000lje@naver.com>
2024-02-08 03:13:13 +0900

    :sparkles: Add 마이페이지 게시글 올리기 선택 시 갤러리 이동
    권한 확인 함수 추가 Related to : #77

--- app/src/main/java/com/umc/anddeul/mypage/MyPageFragment.kt
+++ app/src/main/java/com/umc/anddeul/mypage/MyPageFragment.kt
@@ -1,17 +1,35 @@
 package com.umc.anddeul.mypage
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.umc.anddeul.MainActivity
 import com.umc.anddeul.R
 import com.umc.anddeul.databinding.FragmentMypageBinding
+import com.umc.anddeul.home.PermissionDialog
+import com.umc.anddeul.home.PostUploadActivity
 
 class MyPageFragment : Fragment() {
     lateinit var binding: FragmentMypageBinding
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            // 권한이 허용되면 갤러리 액티비티로 이동
+            val postUploadActivity = Intent(activity, PostUploadActivity::class.java)
+            startActivity(postUploadActivity)
+        } else {
+            val permissionDialog = PermissionDialog()
+            permissionDialog.isCancelable = false
+            permissionDialog.show(parentFragmentManager, "permission dialog")
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,14 +52,70 @@ class MyPageFragment : Fragment() {
                 .commitAllowingStateLoss()
         }
 
-        binding.mypageModifyBtn.setOnClickListener {
-            // MyPageModify로 이동
-            (context as MainActivity).supportFragmentManager.beginTransaction()
-                .add(R.id.mypage_layout, MyPageModifyFragment())
-                .addToBackStack(null)
-                .commitAllowingStateLoss()
+        binding.mypageUploadBtn.setOnClickListener {
+            checkPermission()
         }
 
         return binding.root
+    }
+
+    fun checkPermission() {
+        val permissionImages = android.Manifest.permission.READ_MEDIA_IMAGES
+        val permissionVideos = android.Manifest.permission.READ_MEDIA_VIDEO
+        val permissionUserSelected = android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+        val permissionReadExternal = android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+        val permissionImagesGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            permissionImages
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val permissionVideosGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            permissionVideos
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val permissionUserSelectedGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            permissionUserSelected
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val permissionReadExternalGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            permissionReadExternal
+        ) == PackageManager.PERMISSION_GRANTED
+
+        // SDK 34 이상
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            if (permissionImagesGranted && permissionVideosGranted && permissionUserSelectedGranted) {
+                // 이미 권한이 허용된 경우 해당 코드 실행
+                val postUploadActivity = Intent(activity, PostUploadActivity::class.java)
+                startActivity(postUploadActivity)
+            } else {
+                // 권한이 없는 경우 권한 요청
+                permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED)
+            }
+        }
+
+        // 안드로이드 SDK가 33 이상인 경우
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (permissionImagesGranted && permissionVideosGranted) {
+                // 이미 권한이 허용된 경우 해당 코드 실행
+                val postUploadActivity = Intent(activity, PostUploadActivity::class.java)
+                startActivity(postUploadActivity)
+            } else {
+                // 권한이 없는 경우 권한 요청
+                permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+            }
+        } else { // 안드로이드 SDK가 33보다 낮은 경우
+            if (permissionReadExternalGranted) {
+                // 이미 권한이 허용된 경우 해당 코드 실행
+                val postUploadActivity = Intent(activity, PostUploadActivity::class.java)
+                startActivity(postUploadActivity)
+            } else {
+                // 권한이 없는 경우 권한 요청
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
     }
 }
