5754d95d8fd9b20d19313afc628701d159a9938b
Jieun <lje2000lje@naver.com>
2024-02-13 04:26:00 +0900

    :sparkles: Add 유저 프로필 각 게시글 조회 api 연결
    Related to : #76

--- app/src/main/java/com/umc/anddeul/home/UserProfileRVAdapter.kt
+++ app/src/main/java/com/umc/anddeul/home/UserProfileRVAdapter.kt
@@ -7,7 +7,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.umc.anddeul.databinding.FragmentUserProfilePostImageBinding
 
-class UserProfileRVAdapter(private val userPostList : List<String>?) : RecyclerView.Adapter<UserProfileRVAdapter.UserProfileViewHolder>() {
+class UserProfileRVAdapter(private val userPostList : List<String>?, private val postIdList: List<Int>?) : RecyclerView.Adapter<UserProfileRVAdapter.UserProfileViewHolder>() {
+
+    interface MyItemClickInterface {
+        fun onItemClick(postIdx: Int)
+    }
+
+    private lateinit var mItemClickInterface: MyItemClickInterface
+
+    fun setMyItemClickListener(itemClickListener : MyItemClickInterface) {
+        mItemClickInterface = itemClickListener
+    }
+
     inner class UserProfileViewHolder(val binding: FragmentUserProfilePostImageBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(image: String) {
             Log.e("UserProfileBind", "bind image : ${image}")
@@ -34,6 +45,13 @@ class UserProfileRVAdapter(private val userPostList : List<String>?) : RecyclerV
         } else {
             // userPostList가 null이면 userProfilePostIv 안보이게 설정
             holder.binding.userProfilePostIv.visibility = View.GONE
+        }
+
+        val postIdx = postIdList?.getOrNull(position)
+        holder.itemView.setOnClickListener {
+            if (postIdx != null) {
+                mItemClickInterface.onItemClick(postIdx)
+            }
         }
     }
 
