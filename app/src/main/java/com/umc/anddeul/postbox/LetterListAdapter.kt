e9d30cc9ad8f2a2b03ae8e6a1369450ee2dca7f3
dokyung-kang <kangdk02@naver.com>
2024-02-13 14:34:09 +0900

    :sparkles: Add 날짜별 편지 페이지 편지 리스트 api 연결
    날짜별 편지 보기 페이지에서 보이는 편지리스트 api 연결 Related to : #86

--- app/src/main/java/com/umc/anddeul/postbox/LetterListAdapter.kt
+++ app/src/main/java/com/umc/anddeul/postbox/LetterListAdapter.kt
@@ -5,9 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.umc.anddeul.databinding.ItemLetterlistBinding
+import com.umc.anddeul.postbox.model.Post
 
 class LetterListAdapter : RecyclerView.Adapter<LetterListAdapter.LetterListHolder>() {
-    var letters: List<Letter>? = null
+    var letters: List<Post>? = null
 
     override fun getItemCount(): Int {
         return letters?.size ?: 0
@@ -19,16 +20,16 @@ class LetterListAdapter : RecyclerView.Adapter<LetterListAdapter.LetterListHolde
     }
 
     override fun onBindViewHolder(holder: LetterListHolder, position: Int) {
-        if (letters?.get(position)?.type == 0){
+        if (letters?.get(position)?.voice == 0.toLong()){
             holder.itemBinding.letterIv.visibility = View.VISIBLE
             holder.itemBinding.recordIv.visibility = View.GONE
         }
-        if (letters?.get(position)?.type == 1){
+        if (letters?.get(position)?.voice == 1.toLong()){
             holder.itemBinding.letterIv.visibility = View.GONE
             holder.itemBinding.recordIv.visibility = View.VISIBLE
         }
-        holder.itemBinding.userNmTv.text = letters?.get(position)?.name.toString()
-        holder.itemBinding.detailTv.text = letters?.get(position)?.content.toString()
+        holder.itemBinding.userNmTv.text = letters?.get(position)?.senderIdx
+//        holder.itemBinding.detailTv.text = letters?.get(position)?.content.toString()
         holder.itemBinding.root.setOnClickListener{
             itemClickListener?.onItemClickListener(it, position)
             true
