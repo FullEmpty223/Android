0fc2329ce853db3dfaf6a6a46a698af183a2cfc2
dokyung-kang <kangdk02@naver.com>
2024-02-15 01:09:22 +0900

    :lipstick: Update 그룹 생성 후 페이지
    그룹 생성 후 메인으로 넘어가는 페이지 UI 수정 Related to : #87

--- app/src/main/java/com/umc/anddeul/invite/CreateGroupCodeActivity.kt
+++ app/src/main/java/com/umc/anddeul/invite/CreateGroupCodeActivity.kt
@@ -2,8 +2,10 @@ package com.umc.anddeul.invite
 
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.umc.anddeul.MainActivity
 import com.umc.anddeul.databinding.ActivityCreateGroupCodeBinding
 
 class CreateGroupCodeActivity : AppCompatActivity() {
@@ -29,6 +31,12 @@ class CreateGroupCodeActivity : AppCompatActivity() {
             val clipboard: ClipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
             val clip = ClipData.newPlainText("가족코드", binding.groupCode.text.toString())
             clipboard.setPrimaryClip(clip)
+        }
+        
+        //// 메인 페이지로 이동
+        binding.gotoStartBtn.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
         }
 
     }
