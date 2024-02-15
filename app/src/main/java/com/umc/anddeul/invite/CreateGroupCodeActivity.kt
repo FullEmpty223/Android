bee69935f9568f6bc647a93fdef2d7d4822dddd2
dokyung-kang <kangdk02@naver.com>
2024-02-15 21:32:56 +0900

    :sparkles: Add 가족 초대코드 공유
    가족 초대 공유 기능 추가 Related to : #101

--- app/src/main/java/com/umc/anddeul/invite/CreateGroupCodeActivity.kt
+++ app/src/main/java/com/umc/anddeul/invite/CreateGroupCodeActivity.kt
@@ -25,6 +25,14 @@ class CreateGroupCodeActivity : AppCompatActivity() {
 
         //// 카카오톡으로 전송하기
         binding.sendCodeBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND_MULTIPLE)
+            intent.type = "text/plain"
+            val groupCodeText = intent.getStringExtra("FAMILY_GROUP_CODE")
+            val content = "가족이 초대링크를 공유했어요!"
+            intent.putExtra(Intent.EXTRA_TEXT,"$content\n\n$groupCodeText")
+            val chooserTitle = "가족에게 공유하기"
+            startActivity(Intent.createChooser(intent, chooserTitle))
+
         }
         
         //// 복사하기
