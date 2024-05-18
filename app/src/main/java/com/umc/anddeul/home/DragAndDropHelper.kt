5ba7a5530124513d30a453dd946a84df15feff54
Jieun <lje2000lje@naver.com>
2024-05-19 04:04:39 +0900

    :sparkles: Add DragAndDropHelper 추가
    Related to : #185

--- app/src/main/java/com/umc/anddeul/home/DragAndDropHelper.kt
+++ app/src/main/java/com/umc/anddeul/home/DragAndDropHelper.kt
@@ -1,4 +1,29 @@
 package com.umc.anddeul.home
 
-class DragAndDropHelper {
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+class DragAndDropHelper(private val listener: OnItemMovedListener) : ItemTouchHelper.Callback() {
+
+    interface OnItemMovedListener {
+        fun onItemMoved(fromPosition: Int, toPosition: Int)
+    }
+
+    override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+        val dragFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        return makeMovementFlags(dragFlags, 0)
+    }
+
+    // 아이템을 이동할 때
+    override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        listener.onItemMoved(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
+        return true
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        // 스와이프 동작을 처리할 때
+    }
+
+    override fun isLongPressDragEnabled(): Boolean = true
+
 }
